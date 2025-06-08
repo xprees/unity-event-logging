2a921d94348f2be96281eec227f55c130b755343
Vaclav Bily <xbily@fi.muni.cz>
2025-06-08 23:09:57 +0200

    Add PackageSetup script which defines automagically ScriptDefine indicating other packages enabled event logging

---
+++ Editor/PackageSetup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Xprees.EventLogging.Editor
+{
+    /// This class is responsible for setting up the package's XPREES_EVENT_LOGGING ScriptDefineSymbol in the Project Settings.
+    public static class PackageSetup
+    {
+        private const string packageName = "cz.xprees.event-logging";
+        private const string eventLoggingDefineSymbol = "XPREES_EVENT_LOGGING";
+
+        /// This method checks shows the usage, by checking if the symbol is already defined in the project settings
+        private static bool IsSymbolDefined()
+        {
+#if XPREES_EVENT_LOGGING
+            return true;
+#else
+            return false;
+#endif
+        }
+
+
+        [InitializeOnLoadMethod]
+        private static void Initialize()
+        {
+            var isAlreadyInitialized = IsSymbolDefined() || ScriptDefineUtility.ContainsDefineSymbol(eventLoggingDefineSymbol);
+            if (isAlreadyInitialized) return;
+
+            ScriptDefineUtility.AddDefineSymbol(eventLoggingDefineSymbol);
+            Debug.Log($"Package {packageName} has been initialized. Define symbol '{eventLoggingDefineSymbol}' added to the project settings.");
+        }
+
+    }
+}
---
+++ Editor/ScriptDefineUtility.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEditor.Build;
+
+namespace Xprees.EventLogging.Editor
+{
+    public static class ScriptDefineUtility
+    {
+        public static NamedBuildTarget[] AllTargets => new[]
+        {
+            NamedBuildTarget.Standalone,
+            NamedBuildTarget.Android,
+            NamedBuildTarget.WebGL,
+            NamedBuildTarget.iOS,
+            NamedBuildTarget.tvOS,
+            NamedBuildTarget.XboxOne,
+            NamedBuildTarget.PS4,
+            NamedBuildTarget.PS5,
+            NamedBuildTarget.NintendoSwitch,
+            NamedBuildTarget.WindowsStoreApps,
+            NamedBuildTarget.EmbeddedLinux,
+            NamedBuildTarget.Server,
+            NamedBuildTarget.LinuxHeadlessSimulation,
+            NamedBuildTarget.Unknown,
+            NamedBuildTarget.VisionOS,
+            NamedBuildTarget.QNX,
+        };
+
+        /// Adds the given scripting define symbol to all named build targets.
+        public static void AddDefineSymbol(string symbol)
+        {
+            foreach (var buildTarget in AllTargets)
+            {
+                PlayerSettings.GetScriptingDefineSymbols(buildTarget, out var defines);
+                if (defines.Contains(symbol)) continue;
+
+                var updatedDefines = defines.Concat(new[] { symbol }).ToArray();
+                PlayerSettings.SetScriptingDefineSymbols(buildTarget, updatedDefines);
+            }
+        }
+
+        /// Checks if the given scripting define symbol is present in all/selected named build targets.
+        public static bool ContainsDefineSymbol(string symbol, NamedBuildTarget[] targets = null)
+        {
+            foreach (var buildTarget in targets ?? AllTargets)
+            {
+                PlayerSettings.GetScriptingDefineSymbols(buildTarget, out var defines);
+                if (!defines.Contains(symbol)) return false;
+            }
+
+            return true;
+        }
+
+        /// Removes the given scripting define symbol from all named build targets.
+        public static void RemoveDefineSymbol(string symbol)
+        {
+            foreach (var buildTarget in AllTargets)
+            {
+                PlayerSettings.GetScriptingDefineSymbols(buildTarget, out var defines);
+                if (!defines.Contains(symbol)) continue;
+
+                var updatedDefines = defines.Where(d => d != symbol).ToArray();
+                PlayerSettings.SetScriptingDefineSymbols(buildTarget, updatedDefines);
+            }
+        }
+    }
+}
